dc9022e6c2cf52258ab7a1b3c480f498920603ea
Logan <cavin.namo@qf1tqu1x124p4tlxkq.gq>
2019-03-29 12:15:16 -0400

    added bash script

---
+++ DeleteContainer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# does not need .sh extention
+
+# Delete container if it is running
+containers=$(docker ps -a)
+echo $containers
+FileToFind=$1
+
+echo "$(docker ps -q -f name="$FileToFind")"
+
+if [ "$(docker ps -a -q -f name=$FileToFind)" ]; then
+    echo "Container exists with the name $FileToFind"
+
+    # Check if the status is exited
+    if [ "$(docker ps -aq -f status=exited -f name=$FileToFind)" ]; then
+        echo "container with name $FileToFind is exited"
+              
+     #check if container is running
+    elif [ "$(docker ps -aq -f status=running -f name=$FileToFind)" ]; then
+        echo "container with  name $FileToFind is running"
+        command="docker stop $FileToFind"
+        eval $command
+        echo "$FileToFind has been stopped"
+   else 
+        echo "some other state"
+    fi 
+else 
+    echo "Container does NOT exist with name $FileToFind"
+    # the container doesn't exist to begin with
+    exit   
+fi
+
+# The container is now stopped, so remove it
+command="docker rm $FileToFind"
+eval $command 
+echo "$FileToFind has been removed" 
+
+
